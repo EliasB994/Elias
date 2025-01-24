8f9f5cd687d4ce83cb44ea7c30a39cbf22de0111
EliasB994 <eliasbielskis08@gmail.com>
2025-01-24 15:22:16 +0000

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,8 @@
+const title = document.getElementById('title');
 
+document.addEventListener('mousemove', (e) => {
+    const x = (e.clientX / window.innerWidth - 0.5) * 10; // Adjust parallax intensity
+    const y = (e.clientY / window.innerHeight - 0.5) * 10;
+
+    title.style.transform = `translate(${x}px, ${y}px)`;
+});
