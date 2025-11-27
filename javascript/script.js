94f902dde26369438cb2466436a0471dea6da58a
EliasB994 <eliasbielskis08@gmail.com>
2025-11-27 20:51:15 +0000

    Delete javascript directory

--- javascript/script.js
+++
@@ -1,8 +0,0 @@
-const title = document.getElementById('title');
-
-document.addEventListener('mousemove', (e) => {
-    const x = (e.clientX / window.innerWidth - 0.5) * 10; // Adjust parallax intensity
-    const y = (e.clientY / window.innerHeight - 0.5) * 10;
-
-    title.style.transform = `translate(${x}px, ${y}px)`;
-});
